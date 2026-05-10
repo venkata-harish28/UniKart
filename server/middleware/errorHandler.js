03c81a43d219aad35a8677b3390e1bd249b56369
venkata-harish28 <harishmartha19@gmail.com>
2026-05-10 15:25:01 +0530

    commit

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,4 @@
+﻿module.exports = (err, req, res, next) => {
+  const status = err.statusCode || 500;
+  res.status(status).json({ message: err.message || 'Server Error' });
+};
---
+++ server/routes/wishlist.js
@@ -0,0 +1,2 @@
+﻿const router = require('express').Router();
+module.exports = router;
